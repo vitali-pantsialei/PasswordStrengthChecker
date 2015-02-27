587dbd37410b1cdfdb172f1539041f1202825420
Vitali Pantsialei <bitvitalby@gmail.com>
2015-02-27 21:25:10 +0300

    add tests

---
+++ PasswordStrengthChecker/PasswordChecker.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PasswordStrengthChecker
+{
+    public class PasswordChecker
+    {
+        public bool Verify(string login, string pwd, bool isAdmin, out string msg)
+        {
+            msg = "OK";
+            if (login == null)
+            {
+                msg = "Login is null";
+                return false;
+            }
+            else if (pwd == null)
+            {
+                msg = "Password is null";
+                return false;
+            }
+            else if (pwd.Length <= 7)
+            {
+                msg = "Password length should be more than 7 chars";
+                return false;
+            }
+            else if (!pwd.Any(ch => Char.IsLetter(ch)))
+            {
+                msg = "Password should contain at least 1 letter";
+                return false;
+            }
+            else if (!pwd.Any(ch => Char.IsDigit(ch)))
+            {
+                msg = "Password should contain at least 1 digit";
+                return false;
+            }
+            else if (isAdmin)
+            {
+                if (pwd.Length <= 10)
+                {
+                    msg = "Password should be more  than 10 chars for admins";
+                    return false;
+                }
+                //????????????????else if (!pwd.Any(ch => Char))
+            }
+            return true;
+        }
+    }
+}
---
+++ PasswordStrengthCheckerTest/UnitTest1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PasswordStrengthChecker;
+
+namespace PasswordStrengthCheckerTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void VerifyTest_If_Login_Null_ReturnFalse()
+        {
+            PasswordChecker pc = new PasswordChecker();
+            string msg;
+
+            bool res = pc.Verify(null, "dgdf", false, out msg);
+
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void VerifyTest_If_Password_Null_ReturnFalse()
+        {
+            PasswordChecker pc = new PasswordChecker();
+            string msg;
+
+            bool res = pc.Verify("fdgd", null, false, out msg);
+
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void VerifyTest_If_Login_And_Password_Null_ReturnFalse()
+        {
+            PasswordChecker pc = new PasswordChecker();
+            string msg;
+
+            bool res = pc.Verify(null, null, false, out msg);
+
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void VerifyTest_If_Password_Length_Less_7_ReturnFalse()
+        {
+            PasswordChecker pc = new PasswordChecker();
+            string msg;
+
+            bool res = pc.Verify("user", "mypwd", false, out msg);
+
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void VerifyTest_If_Password_Not_Contain_AtLeast_1_Alpha_ReturnFalse()
+        {
+            PasswordChecker pc = new PasswordChecker();
+            string msg;
+
+            bool res = pc.Verify("user", "1473434534534534534", false, out msg);
+
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void VerifyTest_If_Password_Not_Contain_AtLeast_1_Digit_ReturnFalse()
+        {
+            PasswordChecker pc = new PasswordChecker();
+            string msg;
+
+            bool res = pc.Verify("user", "dsfdsdssfdsgddsfds", false, out msg);
+
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void VerifyTest_If_Password_Length_Less_10_ForAdmins_ReturnFalse()
+        {
+            PasswordChecker pc = new PasswordChecker();
+            string msg;
+
+            bool res = pc.Verify("user", "12345678g", true, out msg);
+
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void VerifyTest_If_Password_NotContain_AtLeast_1_Special_Char_ForAdmins_ReturnFalse()
+        {
+            PasswordChecker pc = new PasswordChecker();
+            string msg;
+
+            bool res = pc.Verify("user", "12345678g6876786", true, out msg);
+
+            Assert.IsFalse(res);
+        }
+    }
+}
